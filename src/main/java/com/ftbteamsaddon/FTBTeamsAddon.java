1f4787d9fe0b98ce6481c1b34ffc4822536ad016
Mathew <61755724+Mathew-1991@users.noreply.github.com>
2025-11-09 00:59:53 -0600

    Create FTBTeamsAddon.java

---
+++ src/main/java/com/ftbteamsaddon/FTBTeamsAddon.java
@@ -0,0 +1,22 @@
+package com.ftbteamsaddon;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("ftbteamsaddon")
+public class FTBTeamsAddon {
+    public static final String MOD_ID = "ftbteamsaddon";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public FTBTeamsAddon() {
+        LOGGER.info("FTB Teams Addon initializing...");
+        
+        // Register event handlers
+        MinecraftForge.EVENT_BUS.register(new ChatEventHandler());
+        MinecraftForge.EVENT_BUS.register(new TabListEventHandler());
+        
+        LOGGER.info("FTB Teams Addon initialized successfully!");
+    }
+}
